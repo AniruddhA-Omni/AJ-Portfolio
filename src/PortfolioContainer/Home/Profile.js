949e3185812f678fcaf3d509871fb211a6a68af2
AniruddhA-Omni <aniruddha.jana2020@vitstudent.ac.in>
2022-10-24 14:42:02 +0530

    Added social media links

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Profile() {
+    return(
+        <div className='profile-container'>
+            <div className = 'profile-parent'>
+                <div className = 'profile-details'>
+                    <div className = 'colz'>
+                    <a href="https://www.linkedin.com/in/aniruddha-jana-aj/">
+                        <i className='fa fa-linkedin-square'></i>
+                    </a>
+                    <a href="https://www.instagram.com/_anirudh.jana_/">
+                        <i className='fa fa-instagram'></i>
+                    </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+    
+}
